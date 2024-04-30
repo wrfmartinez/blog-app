3ea11fb63466d383e511a3d98f830322f8f6bcca
wrfmartinez <wrmartinez709@gmail.com>
2024-04-30 19:19:49 -0400

    Fix issue with being able to edit other users posts and delete them and remove extra whitespace in server.js

--- server.js
+++ server.js
@@ -19,14 +19,10 @@ app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 
 app.use(session({
-    secret: "blog-app-cookies",
+    secret: process.env.SECRET_SESSION,
     resave: false,
     saveUninitialized: true,
 }));
-
-
-
-
 
 app.get('/account/login' , (req, res) => {
     res.render('./account/login.ejs', {
@@ -39,20 +35,22 @@ app.get('/account/new', (req, res) => {
         user: req.session.user
     } )
 })
+
 app.get('/post/new', (req, res) => {
     res.render('./post/new.ejs', {
         user: req.session.user
     })
 })
+
 app.get('/post/:postId/edit' , async(req, res) => {
     const foundPost = await Post.findById(req.params.postId);
-
 
     res.render('./post/edit.ejs', {
         user: req.session.user,
         post: foundPost,
     })
 })
+
 app.post('/account/new', async (req, res) => {
     const account = await User.create(req.body);
     res.redirect('/');
@@ -69,8 +67,8 @@ app.post('/account/login' ,async (req, res) => {
     }
 
     res.redirect('/');
-
 })
+
 app.get('/' , async(req, res) => {
     const posts = await Post.find({});
     const users = await User.find({});
@@ -90,8 +88,8 @@ app.post('/post/new', async (req, res) => {
     });
     res.redirect('/');
 })
-app.delete('/post/:postId', async (req, res) => {
 
+app.delete('/post/:postId', async (req, res) => {
     const selectPost = await Post.findById(req.params.postId);
 
    if(req.session.user){
@@ -101,9 +99,8 @@ app.delete('/post/:postId', async (req, res) => {
    }
 
     res.redirect('/');
-
-
 })
+
 app.get('/sign-out', (req, res) => {
     req.session.destroy();
     res.redirect('/')
@@ -116,6 +113,7 @@ app.get('/account/:accountId/show', async (req, res) => {
         user: user
     })
 })
+
 app.put('/post/:postId/edit', async (req, res) => {
     const selectPost = await Post.findById(req.params.postId);
 
@@ -125,12 +123,9 @@ app.put('/post/:postId/edit', async (req, res) => {
         }
    }
     res.redirect('/');
-
 })
 
-
-
-
-app.listen('3000', () => {
-    console.log('the express app is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`the express app is running on ${PORT}`);
 })
